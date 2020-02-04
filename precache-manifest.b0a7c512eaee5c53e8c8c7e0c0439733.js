2ae3d4d9de6ba8f4708af52b7d8f5974401c34b9
Slava <Mr-93@mail.ru>
2020-02-04 13:31:30 +0300

    Updates

---
+++ precache-manifest.b0a7c512eaee5c53e8c8c7e0c0439733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a02aa43c03df720650fcfcd5bb3bb2d",
+    "url": "/CityBikes/index.html"
+  },
+  {
+    "revision": "284a745bb5d0ffb38752",
+    "url": "/CityBikes/static/css/main.68645efc.chunk.css"
+  },
+  {
+    "revision": "b4af5e3796a1b8be2017",
+    "url": "/CityBikes/static/js/2.43ec29d2.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/CityBikes/static/js/2.43ec29d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284a745bb5d0ffb38752",
+    "url": "/CityBikes/static/js/main.acc86aeb.chunk.js"
+  },
+  {
+    "revision": "fcb0955428aae8d849c5",
+    "url": "/CityBikes/static/js/runtime-main.7104e289.js"
+  }
+]);
